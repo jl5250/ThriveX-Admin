2b36acde280a381223394a8a3326ed241d81776b
宇阳 <liuyuyang1024@yeah.net>
2025-12-26 21:28:04 +0800

    feat: 更新 Header 和 Sidebar 组件，添加 PageTab 组件并优化样式
    - 在 Header 组件中引入 PageTab 以增强导航功能。
    - 调整 Header 和 Sidebar 组件的样式，提升视觉效果和用户体验。
    - 更新导入路径，确保代码一致性。

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,13 +2,14 @@ import { Link } from 'react-router-dom';
 import DropdownUser from './DropdownUser';
 import DarkModeSwitcher from './DarkModeSwitcher';
 import logo from '/logo.png';
+import PageTab from '../PageTab';
 
 const Header = (props: { sidebarOpen: string | boolean | undefined; setSidebarOpen: (arg0: boolean) => void }) => {
   return (
-    <header className="sticky top-0 z-[999] flex w-full bg-light-gradient dark:bg-dark-gradient drop-shadow-1 dark:drop-shadow-none backdrop-blur-lg">
-      <div className="flex flex-grow items-center justify-between px-4 py-3 shadow-2 md:px-6 2xl:px-11">
-        <div className="flex items-center">
-          <div className="flex items-center gap-4 lg:hidden">
+    <header className="sticky top-0 z-[99] flex w-full bg-light-gradient dark:bg-dark-gradient drop-shadow-1 dark:drop-shadow-none">
+      <div className="flex flex-grow items-center justify-between px-4 py-3 shadow-2 md:px-6 2xl:px-11 backdrop-blur-2xl">
+        <div className="flex items-center gap-4 flex-1 min-w-0">
+          <div className="flex items-center gap-4 lg:hidden flex-shrink-0">
             <button
               aria-controls="sidebar"
               onClick={(e) => {
@@ -35,13 +36,15 @@ const Header = (props: { sidebarOpen: string | boolean | undefined; setSidebarOp
             </Link>
           </div>
 
-          <div className="hidden xs:block ml-5 2xl:ml-0">有些梦虽然遥不可及，但并不是不可能实现</div>
+          <div className="flex-1 min-w-0">
+            <PageTab />
+          </div>
         </div>
 
-        <div className="flex items-center gap-3 2xsm:gap-7">
+        <div className="flex items-center gap-3 2xsm:gap-7 flex-shrink-0 ml-4">
           <ul className="flex items-center gap-2 2xsm:gap-4">
             <li className="hidden md:block">
-              <a href="https://github.com/LiuYuYang01/ThriveX-Admin" target="_blank" className="hover:text-primary text-sm" rel="noreferrer">
+              <a href="https://github.com/LiuYuYang01/ThriveX-Admin" target="_blank" className="text-gray-500 dark:text-gray-400 hover:!text-primary text-sm" rel="noreferrer">
                 开源不易，赏个 Star 吧！！！
               </a>
             </li>
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -235,9 +235,9 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
 
   // 渲染侧边栏组件
   return (
-    <aside ref={sidebar} className={`absolute left-0 top-0 z-[99] flex h-screen w-64 flex-col overflow-y-hidden duration-300 ease-linear lg:static lg:translate-x-0 ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'} ${isSideBarTheme === 'dark' ? 'bg-black dark:bg-boxdark' : 'bg-light-gradient dark:bg-dark-gradient border-r border-stroke dark:border-strokedark transition-all'}`}>
+    <aside ref={sidebar} className={`absolute left-0 top-0 z-[999] flex h-screen w-64 flex-col overflow-y-hidden duration-300 ease-linear lg:static lg:translate-x-0 ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'} ${isSideBarTheme === 'dark' ? 'bg-black dark:bg-boxdark' : 'bg-light-gradient dark:bg-dark-gradient border-r border-stroke dark:border-strokedark transition-all backdrop-blur-2xl'}`}>
       {/* Logo 和标题区域 */}
-      <div className="flex justify-center items-center gap-2 px-6 py-5.5 pb-0 lg:pt-6">
+      <div className="flex justify-center items-center gap-2 px-6 py-5 pb-0 lg:pt-6">
         <NavLink to="/" className={`flex items-center ${isSideBarTheme === 'dark' ? 'font-bold text-white' : 'text-[#555] dark:text-white'}`}>
           <img src={logo} alt="logo" className="w-8 mr-2.5" />
           <div>Thrive X</div>
